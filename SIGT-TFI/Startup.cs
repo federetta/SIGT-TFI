d403cb0d6a766fcc1c402b4a63b6efeaa722e7df
Federico Retta <fretta@tmdsa.loc>
2018-08-22 12:00:04 -0300

    Login

---
+++ SIGT-TFI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIGT_TFI.Startup))]
+namespace SIGT_TFI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
