fa58d12f12d8fac314adef516b3db3bb6b952a49
Federico Retta <federico.retta@gmail.com>
2018-08-29 19:58:30 -0300

    DataAccessComponent

--- DAL/DALEmpresa.cs
+++ DAL/DALEmpresa.cs
@@ -7,14 +7,16 @@ using Services;
 using Entities;
 using System.Data.SqlClient;
 using System.Data;
+using Microsoft.Practices.EnterpriseLibrary.Data;
 
 namespace DAL
 {
-    public class DALEmpresa
+    public class DALEmpresa : DataAccessComponent
     {
         Services.ConexionSQL conexion = new Services.ConexionSQL();
         public Entities.Empresa InsertarEmpresa(Entities.Empresa empresa)
         {
+            var db = DatabaseFactory.CreateDatabase("Data Source = frdell\\sqlexpress; Initial Catalog = SIGT_Web; Integrated Security = True");
             try
             {
                 conexion.ConectarBaseDatos();
@@ -24,10 +26,12 @@ namespace DAL
                 cmd.Parameters.Add("@NombreFantasia_empresa", SqlDbType.NVarChar).Value = empresa.NombreFantasia;
                 cmd.Parameters.Add("@Cuit_empresa", SqlDbType.NVarChar).Value = empresa.Cuit;
                 cmd.Parameters.Add("@TipoContribuyente_empresa", SqlDbType.Int).Value = empresa.TipoContribuyente.id;
-                cmd.Parameters.Add("@TipoEmpresa_empresa", SqlDbType.Int).Value = empresa.Tipo_Empresa.id;
+                cmd.Parameters.Add("@Tipo_empresa", SqlDbType.Int).Value = empresa.Tipo_Empresa.id;
                 
                 // Declaro el ID para retornarlo en el textbox
-                cmd.Parameters.Add("@id_empresa", SqlDbType.Int).Direction = ParameterDirection.Output;
+                //cmd.Parameters.Add("@id_empresa", SqlDbType.Int).Direction = ParameterDirection.Output;
+                empresa.id = Convert.ToInt32(db.ExecuteScalar(cmd));
+
                 cmd.ExecuteNonQuery();
                 // Retorno el ID_empresa
                 //empresa.id = Convert.ToInt32(cmd.Parameters("@id_empresa").Value);
@@ -50,6 +54,93 @@ namespace DAL
             cmd.Parameters.Add(parametro);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="product"></param>
+        public void UpdateById(Empresa empresa)
 
+          
+        {
+            const string sqlStatement = "UPDATE dbo.Empresa " +
+                "SET [RazonSocial]=@razonSocial_empresa, " +
+                    "[NombreFantasia_empresa]=@NombreFantasia_empresa, " +
+                    "[DealerId]=@Cuit_Empresa, " +
+                    "[TipoContribuyente_empresa]=@TipoContribuyente_empresa, " +
+                    "[Tipo_empresa]=@Tipo_empresa, " +
+                    "[QuantitySold]=@QuantitySold, " +
+                    "[ChangedOn]=@ChangedOn, " +
+                    "[ChangedBy]=@ChangedBy " +
+                "WHERE [Id]=@Id_empresa ";
+
+            var db = DatabaseFactory.CreateDatabase(ConnectionName);
+            using (var cmd = db.GetSqlStringCommand(sqlStatement))
+            {
+                db.AddInParameter(cmd, "@razonSocial_empresa", DbType.String, empresa.RazonSocial);
+                db.AddInParameter(cmd, "@NombreFantasia_empresa", DbType.String, empresa.NombreFantasia);
+                db.AddInParameter(cmd, "@Cuit_Empresa", DbType.Int32, empresa.Cuit);
+                db.AddInParameter(cmd, "@TipoContribuyente_empresa", DbType.String, empresa.TipoContribuyente.id);
+                db.AddInParameter(cmd, "@Tipo_empresa", DbType.Decimal, empresa.Tipo_Empresa.id);
+                db.AddInParameter(cmd, "@Id_empresa", DbType.Int32, empresa.id);
+                db.ExecuteNonQuery(cmd);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteById(int id)
+        {
+            const string sqlStatement = "DELETE dbo.Empresa WHERE [Id]=@Id ";
+            var db = DatabaseFactory.CreateDatabase(ConnectionName);
+            using (var cmd = db.GetSqlStringCommand(sqlStatement))
+            {
+                db.AddInParameter(cmd, "@Id_empresa", DbType.Int32, id);
+                db.ExecuteNonQuery(cmd);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>		
+        public List<Empresa> Select()
+        {
+            // WARNING! Performance
+            const string sqlStatement = "SELECT [Id_empresa], [razonSocial_empresa], [NombreFantasia_empresa], [Cuit_Empresa], [TipoContribuyente_empresa], [Tipo_empresa]  FROM dbo.Empresa ";
+
+            var result = new List<Empresa>();
+            var db = DatabaseFactory.CreateDatabase(ConnectionName);
+            using (var cmd = db.GetSqlStringCommand(sqlStatement))
+            {
+                using (var dr = db.ExecuteReader(cmd))
+                {
+                    while (dr.Read())
+                    {
+                        var product = LoadEmpresa(dr); // Mapper
+                        result.Add(product);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static Empresa LoadEmpresa(IDataReader dr)
+        {
+            var empresa = new Empresa
+            {
+                id = GetDataValue<int>(dr, "Id_empresa"),
+                RazonSocial = GetDataValue<string>(dr, "razonSocial_empresa"),
+                NombreFantasia = GetDataValue<string>(dr, "Description"),
+                Cuit = GetDataValue<int>(dr, "DealerId"),
+                TipoContribuyente = GetDataValue<int>(dr, "Image"),
+                Tipo_Empresa = GetDataValue<int>(dr, "Price"),
+                
+            };
+
+        
+            return empresa;
+        }
     }
 }
---
+++ DAL/DataAccessComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace DAL
+{
+    /// <summary>
+    /// Base data access component class.
+    /// </summary>
+    public abstract class DataAccessComponent
+    {
+        protected const string ConnectionName = "Data Source=frdell\\sqlexpress;Initial Catalog=SIGT_Web;Integrated Security=True";
+
+        static DataAccessComponent()
+        {
+            // Enterprise Library DAAB 6.0 - Database Factories.
+            DatabaseFactory.SetDatabaseProviderFactory(new DatabaseProviderFactory(), false);
+        }
+
+        //protected int PageSize = Convert.ToInt32(ConfigurationManager.AppSettings["PageSize"]);
+        //protected int PageSize
+        //{
+        //    get
+        //    {
+        //        return Convert.ToInt32(ConfigurationManager.AppSettings["PageSize"]);
+        //    }
+        //}
+
+        protected static T GetDataValue<T>(IDataReader dr, string columnName)
+        {
+            var i = dr.GetOrdinal(columnName);
+
+            if (!dr.IsDBNull(i))
+                return (T)dr.GetValue(i);
+            return default(T);
+        }
+
+        protected string FormatFilterStatement(string filter)
+        {
+            return Regex.Replace(filter, "^(AND|OR)", string.Empty);
+        }
+    }
+}
+
